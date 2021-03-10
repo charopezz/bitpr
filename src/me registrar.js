7868f2ac268f2f3b1189592a6d5fdc95a5f6687b
charopezz <77083065+charopezz@users.noreply.github.com>
2021-03-10 14:51:48 -0300

    Add files via upload

---
+++ src/me registrar.js
@@ -0,0 +1,16 @@
+const me registrar = (prefix, sender) => {
+	return `𝙗𝙚𝙢 𝙫𝙞𝙣𝙙𝙤! 𝙫𝙖𝙢𝙤𝙨 𝙡𝙖 𝙘 𝙧𝙚𝙜𝙞𝙨𝙩𝙧𝙖𝙧☺︎︎
+Primeiro digite /emprego e escolha um depois você cópia esse texto abaixo 
+👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾
+>Nome:
+>PSN:
+>CPF: 
+>Idade:
+>Emprego
+>E modder?:
+==========
+obs:o cpf e so 4 numero
+de um /emprego '
+}
+
+exports.me registrar = me registrar 
